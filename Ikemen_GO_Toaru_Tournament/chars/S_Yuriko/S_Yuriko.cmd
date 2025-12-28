bdecb389174f30fc537f076dee42a85b39c12b06
REBIÈRE Marc <sebastian.rebiere@gmail.com>
2025-12-28 17:39:40 +0300

    4.5.2

--- Ikemen_GO_Toaru_Tournament/chars/S_Yuriko/S_Yuriko.cmd
+++ Ikemen_GO_Toaru_Tournament/chars/S_Yuriko/S_Yuriko.cmd
@@ -1029,16 +1029,12 @@ time = 1
 [State -1, Throw]
 Type = ChangeState
 value = 800
-	TriggerAll=Alive!=0
-	TriggerAll = StateType != A
-	TriggerAll=command="a+b"
-	triggerAll = Var(9) = 0
-	;TriggerAll=command="holdfwd"
-	;TriggerAll=StateNo!=100
-	Triggerall=enemynear,StateType=S||enemynear,StateType=C
-	Triggerall=enemynear,MoveType!=H
-	Trigger1=Ctrl
-
+triggerall = command = "x"
+triggerAll = Var(9) = 0
+triggerall = statetype != A
+triggerall = command = "holdfwd" || command = "holdback" 
+trigger1 = ctrl
+trigger2 = stateno=[100,101]
 ;---------------------------------------------------
 ;[State -1, ìñêg]
 ;	type = ChangeState
